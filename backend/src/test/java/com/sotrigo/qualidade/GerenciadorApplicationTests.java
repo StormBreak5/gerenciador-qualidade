7576edda28c57979d566f73b2477c23caa4fc772
unknown <itzacalbertin@gmail.com>
2025-12-17 13:36:46 -0300

    first commit, initial structure

---
+++ backend/src/test/java/com/sotrigo/qualidade/GerenciadorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sotrigo.qualidade;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GerenciadorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
